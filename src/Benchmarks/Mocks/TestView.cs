7ecdde9d9245c09a35eef46ae00a19aa3f00ec57
RLittlesII <6969701+RLittlesII@users.noreply.github.com>
2019-03-26 22:22:43 -0500

    performance: Added Benchmarks
    - Added ViewStackServiceBenchmarks
    - Added PopModalBenchmarks
    - Added PopPageBenchmarks
    - Added PushModalBenchmarks
    - Added PushPageBenchmarks
    - Added PopToRootPageBenchmarks

---
+++ src/Benchmarks/Mocks/TestView.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) 2019 .NET Foundation and Contributors. All rights reserved.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using ReactiveUI;
+
+namespace Sextant.Benchmarks
+{
+    /// <summary>
+    /// View for the locator.
+    /// </summary>
+    /// <seealso cref="ReactiveUI.IViewFor{Benchmarks.ViewModel}" />
+    public class TestView : IViewFor<ViewModel>
+    {
+        /// <summary>
+        /// Gets or sets the ViewModel corresponding to this specific View. This should be
+        /// a DependencyProperty if you're using XAML.
+        /// </summary>
+        object IViewFor.ViewModel
+        {
+            get => ViewModel;
+            set => ViewModel = (ViewModel)value;
+        }
+
+        /// <summary>
+        /// Gets or sets the ViewModel corresponding to this specific View. This should be
+        /// a DependencyProperty if you're using XAML.
+        /// </summary>
+        public ViewModel ViewModel { get; set; }
+    }
+}
