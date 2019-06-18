425a8f8623d06cef7033cedc7c0b2b41ef7cddf8
RLittlesII <6969701+RLittlesII@users.noreply.github.com>
2019-06-18 18:03:59 -0500

    Added NavigationParameter implementation
    Added unit tests for navigation parameters

--- src/Sextant.Tests/Navigation/ParameterTests.cs
+++ src/Sextant.Tests/Navigation/ParameterTests.cs
@@ -1,10 +1,92 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Sextant.Mocks;
+using Shouldly;
+using Xunit;
 
-namespace Sextant.Tests.Navigation
+namespace Sextant.Tests
 {
-    class ParameterTests
+    /// <summary>
+    /// Test the <see cref="NavigationParameter"/> class.
+    /// </summary>
+    public sealed class ParameterTests
     {
+        /// <summary>
+        /// Tests the WhenNavigatingTo method.
+        /// </summary>
+        public sealed class TheWhenNavigatingToMethod
+        {
+            /// <summary>
+            /// Should unwrap the parameters.
+            /// </summary>
+            [Fact]
+            public void Should_Unwrap_Parameters()
+            {
+                // Given
+                ParameterViewModel sut = new ParameterViewModel();
+
+                // When
+                sut.WhenNavigatingTo(new NavigationParameter { { "hello", "world" } });
+
+                // Then
+                sut.Text.ShouldBe("world");
+            }
+
+            /// <summary>
+            /// Should return null if key not found.
+            /// </summary>
+            [Fact]
+            public void Should_Return_Null_If_Key_Not_Found()
+            {
+                // Given
+                ParameterViewModel sut = new ParameterViewModel();
+
+                // When
+                sut.WhenNavigatingTo(new NavigationParameter { });
+
+                // Then
+                sut.Text.ShouldBeNull();
+            }
+        }
+
+        /// <summary>
+        /// Tests the WhenNavigatedTo method.
+        /// </summary>
+        public sealed class TheWhenNavigatedToMethod
+        {
+            /// <summary>
+            /// Should unwrap the parameters.
+            /// </summary>
+            [Fact]
+            public void Should_Unwrap_Parameters()
+            {
+                // Given
+                ParameterViewModel sut = new ParameterViewModel();
+
+                // When
+                sut.WhenNavigatedTo(new NavigationParameter { { "hello", "world" }, { "life", 42 } });
+
+                // Then
+                sut.Text.ShouldBe("world");
+            }
+
+            /// <summary>
+            /// Should return null if key not found.
+            /// </summary>
+            [Fact]
+            public void Should_Return_Null_If_Key_Not_Found()
+            {
+                // Given
+                ParameterViewModel sut = new ParameterViewModel();
+
+                // When
+                sut.WhenNavigatedTo(new NavigationParameter { });
+
+                // Then
+                sut.Text.ShouldBeNull();
+            }
+        }
     }
 }
--- src/Sextant/Navigation/NavigationParameter.cs
+++ src/Sextant/Navigation/NavigationParameter.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿// Copyright (c) 2019 .NET Foundation and Contributors. All rights reserved.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Sextant.Navigation
+namespace Sextant
 {
-    class NavigationParameter
+#pragma warning disable CA1710, CA2237
+    /// <summary>
+    /// Represents parameters that can be passed during navigation.
+    /// </summary>
+    /// <seealso cref="object" />
+    /// <seealso cref="INavigationParameter" />
+    public class NavigationParameter : Dictionary<string, object>, INavigationParameter
     {
     }
+#pragma warning restore CA1710, CA2237
 }
