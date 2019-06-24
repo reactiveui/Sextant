750247e88234da95165a3b6c01c1c7252e12e26b
Rodney Littles II <6969701+RLittlesII@users.noreply.github.com>
2019-06-24 18:35:10 -0400

    Added Suppress Message attribute

--- src/Sextant/Navigation/NavigationParameter.cs
+++ src/Sextant/Navigation/NavigationParameter.cs
@@ -6,18 +6,19 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 
 namespace Sextant
 {
-#pragma warning disable CA1710, CA2237
     /// <summary>
     /// Represents parameters that can be passed during navigation.
     /// </summary>
     /// <seealso cref="object" />
     /// <seealso cref="INavigationParameter" />
+    [SuppressMessage("Design", "CA1710: Identifiers should have correct suffix.", Justification = "Deliberate usage")]
+    [SuppressMessage("Design", "CA2237: Mark ISerializable types with SerializableAttribute.", Justification = "Deliberate usage")]
     public class NavigationParameter : Dictionary<string, object>, INavigationParameter
     {
     }
-#pragma warning restore CA1710, CA2237
 }
