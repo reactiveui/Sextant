00fb16c28bc94ea3f97a60c8e85008aaf3cf663e
Rodney Littles II <6969701+RLittlesII@users.noreply.github.com>
2019-12-06 14:19:03 -0600

    housekeeping: Clean up analyzer errors and warnings

--- src/Sextant.iOS.Runner/PageUiViewController.cs
+++ src/Sextant.iOS.Runner/PageUiViewController.cs
@@ -1,4 +1,9 @@
-﻿using ReactiveUI;
+﻿// Copyright (c) 2019 .NET Foundation and Contributors. All rights reserved.
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using ReactiveUI;
 
 namespace Sextant.IOS.Runner
 {
@@ -7,7 +12,7 @@ namespace Sextant.IOS.Runner
         object IViewFor.ViewModel
         {
             get => ViewModel;
-            set => ViewModel = (PageViewModelMock) value;
+            set => ViewModel = (PageViewModelMock)value;
         }
 
         public PageViewModelMock ViewModel { get; set; } = new PageViewModelMock();
