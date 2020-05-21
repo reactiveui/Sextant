ab4862a3f487465c01b5a6d602aa8b94878b0316
Rodney Littles II <6969701+RLittlesII@users.noreply.github.com>
2020-05-20 23:10:13 -0500

    fix: Changed PagePopped to use Observable.FromEvent (#224)
    * fix: Changed PagePopped to use Observable.FromEvent
    
    * Removing Select
    
    * fixed public api test
    
    * removed newline

--- src/Sextant.XamForms/NavigationView.cs
+++ src/Sextant.XamForms/NavigationView.cs
@@ -50,9 +50,15 @@ namespace Sextant.XamForms
 
             PagePopped =
                 Observable
-                    .FromEventPattern<NavigationEventArgs>(x => Popped += x, x => Popped -= x)
-                    .Select(ep => ep.EventArgs.Page.BindingContext as IViewModel)
-                    .Where(x => x != null);
+                    .FromEvent<EventHandler<NavigationEventArgs>, object>(
+                        handler =>
+                        {
+                            void Handler(object sender, NavigationEventArgs args) => handler(args.Page.BindingContext);
+                            return Handler;
+                        },
+                        x => Popped += x,
+                        x => Popped -= x)
+                    .Cast<IViewModel>();
         }
 
         /// <summary>
@@ -70,16 +76,22 @@ namespace Sextant.XamForms
 
             PagePopped =
                 Observable
-                    .FromEventPattern<NavigationEventArgs>(x => Popped += x, x => Popped -= x)
-                    .Select(ep => ep.EventArgs.Page.BindingContext as IViewModel)
-                    .Where(x => x != null);
+                    .FromEvent<EventHandler<NavigationEventArgs>, object>(
+                        handler =>
+                        {
+                            void Handler(object sender, NavigationEventArgs args) => handler(args.Page.BindingContext);
+                            return Handler;
+                        },
+                        x => Popped += x,
+                        x => Popped -= x)
+                    .Cast<IViewModel>();
         }
 
         /// <inheritdoc />
         public IScheduler MainThreadScheduler => _mainScheduler;
 
         /// <inheritdoc />
-        public IObservable<IViewModel?> PagePopped { get; }
+        public IObservable<IViewModel> PagePopped { get; }
 
         /// <inheritdoc />
         public IObservable<Unit> PopModal() =>
