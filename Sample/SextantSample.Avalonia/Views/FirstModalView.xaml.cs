a63f4b1e0cf18471a1cc0702dcf0c5e767ff4db7
Artyom <worldbeater-dev@yandex.ru>
2020-10-21 17:30:28 +0300

    Data contexting

--- Sample/SextantSample.Avalonia/Views/FirstModalView.xaml.cs
+++ Sample/SextantSample.Avalonia/Views/FirstModalView.xaml.cs
@@ -1,11 +1,22 @@
-﻿using Avalonia.Markup.Xaml;
+﻿using System.Reactive.Disposables;
+using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
+using ReactiveUI;
 using SextantSample.ViewModels;
 
 namespace SextantSample.Avalonia.Views
 {
     public class FirstModalView : ReactiveUserControl<FirstModalViewModel>
     {
-        public FirstModalView() => AvaloniaXamlLoader.Load(this);
+        public FirstModalView()
+        {
+            AvaloniaXamlLoader.Load(this);
+            this.WhenActivated(disposables =>
+            {
+                this.WhenAnyValue(x => x.ViewModel)
+                    .BindTo(this, x => x.DataContext)
+                    .DisposeWith(disposables);
+            });
+        }
     }
 }
--- Sample/SextantSample.Avalonia/Views/GreenView.xaml.cs
+++ Sample/SextantSample.Avalonia/Views/GreenView.xaml.cs
@@ -1,11 +1,22 @@
-﻿using Avalonia.Markup.Xaml;
+﻿using System.Reactive.Disposables;
+using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
+using ReactiveUI;
 using SextantSample.ViewModels;
 
 namespace SextantSample.Avalonia.Views
 {
     public class GreenView : ReactiveUserControl<GreenViewModel>
     {
-        public GreenView() => AvaloniaXamlLoader.Load(this);
+        public GreenView()
+        {
+            AvaloniaXamlLoader.Load(this);
+            this.WhenActivated(disposables =>
+            {
+                this.WhenAnyValue(x => x.ViewModel)
+                    .BindTo(this, x => x.DataContext)
+                    .DisposeWith(disposables);
+            });
+        }
     }
 }
