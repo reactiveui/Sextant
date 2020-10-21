ae2842ef09d9db3ca56417507c4de273d133526a
Artyom <worldbeater-dev@yandex.ru>
2020-10-21 17:21:02 +0300

    Bring in more sample views

--- Sample/SextantSample.Avalonia/App.xaml.cs
+++ Sample/SextantSample.Avalonia/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Avalonia;
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Sextant;
 using SextantSample.Avalonia.Views;
@@ -13,17 +15,52 @@ namespace SextantSample.Avalonia
 
         public override void OnFrameworkInitializationCompleted()
         {
-            // Register the views.
-            Locator.CurrentMutable
+            Locator
+                .CurrentMutable
+                .RegisterView<HomeView, HomeViewModel>()
+                .RegisterView<FirstModalView, FirstModalViewModel>()
+                .RegisterView<SecondModalView, SecondModalViewModel>()
+                .RegisterView<RedView, RedViewModel>()
                 .RegisterView<GreenView, GreenViewModel>()
-                .RegisterView<FirstModalView, FirstModalViewModel>();
+                .RegisterViewModelFactory(() => new DefaultViewModelFactory())
+                .RegisterNavigationView(() => new NavigationView())
+                .RegisterViewModel(() => new GreenViewModel(Locator.Current.GetService<IViewStackService>()));
 
-            // Push the view model.
-            Locator.Current
+            Locator
+                .Current
                 .GetService<IViewStackService>()
-                .PushPage(new GreenViewModel(Locator.Current.GetService<IViewStackService>()));
+                .PushPage(new HomeViewModel());
 
+            var navigationView = Locator.Current.GetNavigationView();
+            new Window { Content = navigationView }.Show();
             base.OnFrameworkInitializationCompleted();
+        }
+    }
+
+    public static class Workarounds
+    {
+        public static IMutableDependencyResolver RegisterNavigationView<TView>(
+            this IMutableDependencyResolver dependencyResolver, Func<TView> navigationViewFactory)
+            where TView : IView
+        {
+            var navigationView = navigationViewFactory();
+            var viewStackService = new ViewStackService(navigationView);
+
+            dependencyResolver.RegisterLazySingleton<IViewStackService>(() => viewStackService);
+            dependencyResolver.RegisterLazySingleton<IView>(() => navigationView, "NavigationView");
+            return dependencyResolver;
+        }
+
+        public static IView GetNavigationView(
+            this IReadonlyDependencyResolver dependencyResolver,
+            string contract = null)
+        {
+            if (dependencyResolver is null)
+            {
+                throw new ArgumentNullException(nameof(dependencyResolver));
+            }
+
+            return dependencyResolver.GetService<IView>(contract ?? "NavigationView");
         }
     }
 }
---
+++ Sample/SextantSample.Avalonia/Views/RedView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+using SextantSample.ViewModels;
+
+namespace SextantSample.Avalonia.Views
+{
+    public class RedView : ReactiveUserControl<RedViewModel>
+    {
+        public RedView() => AvaloniaXamlLoader.Load(this);
+    }
+}
