a2ea5fda19827bac47cc3f8e545ac4559143fcfe
artyom <worldbeater-dev@yandex.ru>
2021-02-24 02:06:34 +0300

    housekeeping: Update copyright year

--- Sample/SextantSample.UWP/Properties/AssemblyInfo.cs
+++ Sample/SextantSample.UWP/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SextantSample.UWP")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © .NET Foundation 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
