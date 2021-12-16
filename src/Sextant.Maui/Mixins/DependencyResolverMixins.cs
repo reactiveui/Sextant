a8498b0dcbed2bb70f86a0b534be6c8b9d608a07
OmnipotentOwl <1769881+OmnipotentOwl@users.noreply.github.com>
2021-12-16 18:13:41 -0500

    Cleanup usings into global usings for nuget package. Fix name of initialize to reflect maui

--- src/Sextant.Maui/Mixins/DependencyResolverMixins.cs
+++ src/Sextant.Maui/Mixins/DependencyResolverMixins.cs
@@ -3,11 +3,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
 
-using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Reactive.Concurrency;
-using ReactiveUI;
-using Splat;
 
 namespace Sextant.Maui
 {
